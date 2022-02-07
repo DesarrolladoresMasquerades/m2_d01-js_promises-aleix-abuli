748d8813ff51b13527eae5b425f3cfd6d132ba98
Aleix Abulí <aleixabuli088@gmail.com>
2022-02-07 16:13:01 +0100

    finished

--- exercises/01-exe-promises/index.js
+++ exercises/01-exe-promises/index.js
@@ -33,18 +33,43 @@ console.log("Waiting for the fake server to reply...");
 // ----- You have to make it work as intended -----
 const serverResponse = new Promise(responseHandler);
 
-serverResponse.then().catch();
+serverResponse
+.then(updateDOMWithData)
+.catch(updateDOMWithError);
 
 function updateDOMWithData(data) {
-  const html = ``; // build the html for data here
+  const html = // build the html for data here
+    `<div class="container">
+      <article class="product">
+        <img src="${data.img}" alt="">
+        <h3>${data.item}</h3>
+        <h3>$ ${data.price}</h3>
+        <h4>Year: ${data.year}</h4>
+      </article>
+    </div>`;
 
   // Append the HTML to the DOM here
+  const body = document.querySelector('body');
+  const newContainer = document.createElement('div');
+  newContainer.innerHTML = html;
+  body.appendChild(newContainer);
 }
 
 function updateDOMWithError(error) {
-  const html = ``; // build the html for the error here
+  const html = // build the html for the error here
+    `<div class="container error">
+      <article class="product">
+        <h1>ERROR</h1>
+        <p>${error}</p>
+        <img src="https://vignette.wikia.nocookie.net/battlefordreamisland/images/f/f1/Roboty_book.png/revision/latest?cb=20190908174044" alt="">
+      </article>
+    </div>`;
 
   // Append the HTML to the DOM here
+  const body = document.querySelector('body');
+  const newContainer = document.createElement('div');
+  newContainer.innerHTML = html;
+  body.appendChild(newContainer);
 }
 
 function responseHandler(resolveCb, rejectCb) {
@@ -61,8 +86,11 @@ function responseHandler(resolveCb, rejectCb) {
 
   setTimeout(
     () => {
-      // Read the "serverIsUp" flag here
-      // and handle the promise to dispaly the correct HTML into the DOM here.
+      if(serverIsUp){ // Read the "serverIsUp" flag here
+        resolveCb(data);
+      } else {
+        rejectCb(error)
+      } // and handle the promise to dispaly the correct HTML into the DOM here.
     },
 
     1000 + Math.random() * 1000 // This is a random waiting time between 1000 and 2000 ms
--- index.js
+++ index.js
@@ -1,1 +1,132 @@
 console.log("JS Loaded");
+/*
+function standardCallback(){
+    console.log('I have been run in the future' + new Date());
+}
+
+console.log('Time is' + new Date());
+setTimeout(standardCallback, 3 * 1000);
+console.log('Time after one line is' + new Date());
+
+
+const magicButton = document.getElementById('magic-button');
+
+magicButton.onclick = standardCallback;
+*/
+const directions = [
+    "Starting point: Ironhack Miami",
+    "↑ Head east on SW 8th St/Carlos Arboleya toward SW 1st Avenue",
+    "➔ Turn right onto S Miami Ave",
+    "* Chipotle Mexican Grill 891 S Miami Ave, Miami",
+];
+/*
+const directionList = [];
+
+function getDirections(step, returningCallback, errorCallback) {
+    setTimeout(() => {
+    console.log(directions[step]);
+    directionList.push(directions[step]);
+    if (!directions[step]) errorCallback("Instructions not found.");
+    else returningCallback();
+    }, 2000);
+}
+  
+console.log(
+    "First leg of the journey ",
+    getDirections(
+        0,
+        () => 
+            getDirections(
+                1,
+                ()=>
+                    getDirections(
+                        2,
+                        getDirections(
+                            3,
+                            ()=> {
+                                console.log('You arrived'); // Pyramid of Doom
+                            },
+                            ()=>{}
+                        ),
+                        ()=>{}
+                    ),
+                ()=>{}
+            ),
+        () => {
+            console.log("There are no more steps");
+        }
+    )
+);
+*/
+/*
+function obtainDirections(step) {
+    return new Promise((resolvedCb, rejectedCb) => {
+        setTimeout(()=>{
+            console.log(directions[step]);
+            if (!directions[step]) rejectedCb(`Directions not found for step ${step}.`); // You can generate custom errors with promises
+            else resolvedCb(`Directions GIVEN for step ${step}.`);
+        }, 3*1000);
+    });
+}
+
+
+console.log(obtainDirections(0));
+*/
+/*
+obtainDirections(0) // You interrogate the promise to see if it is fulfilled
+    .then(() => obtainDirections(1))
+    .then(() => obtainDirections(2))
+    .then(() => obtainDirections(3))
+    .then(() => obtainDirections(1000))
+    .catch((error) => {
+      console.log('There was an error: ' + error);
+    })
+/*
+const magicButton = document.getElementById('magic-button');
+
+magicButton.onclick = handleMagicClick;
+
+function handleMagicClick() {
+    return obtainDirections(0)
+        .then(()=>obtainDirections(1))
+        .then(()=>obtainDirections(2))
+        .then(()=>obtainDirections(3));
+}
+*/
+
+
+
+
+
+function onclickHandler(){
+    return new Promise((resolve, reject)=>{
+        /**
+         * equivalent to:
+         * let randomResult
+         * if(Math.random()>0.5) randomResult = true
+         * else randomResult = false
+         */
+
+        const randomResult = Math.random() > 0.2 ? true : false
+
+        if (randomResult){
+            resolve('You have another to do!');
+        } else {
+            reject('You ran out of to-dos');
+        }
+    })
+
+    .then((newTodo) =>{
+        const todoList = document.getElementById('to-do-list');
+        todoList.innerHTML += `<div> ${newTodo} </div>`;
+    })
+    
+    .catch((err) => {
+        const todoList = document.getElementById('to-do-list');
+        todoList.innerHTML = `<h3>${err}</h3>`;
+    });
+}
+
+const magicButton = document.getElementById('magic-button');
+
+magicButton.onclick = () => onclickHandler();
